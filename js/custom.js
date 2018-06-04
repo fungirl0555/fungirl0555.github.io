2a375b64a9de418f6009c874fa6c09ca2fb47773
locanlive <locanlive@outlook.com>
2018-06-05 02:22:17 +0600

    project

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+function myFunctionl1() {
+	window.location.replace("http://get.safedatingverified.com/?a=4267&c=443&s1=");
+	return true;
+}
+function myFunctionl2() {
+	window.location.replace("http://have.safemeetings.net/?a=4267&c=427&s1=");
+	return true;
+}
+function myFunctionl3() {
+	window.location.replace("http://have.safemeetings.net/?a=4267&c=538&s1=");
+	return true;
+}
+function myFunctionl4() {
+	window.location.replace("http://have.safemeetings.net/?a=4267&c=85&s1=");
+	return true;
+}
+function myFunctionl4() {
+	window.location.replace("http://my.casualconnect.net/?a=4267&c=64&s1=");
+	return true;
+}
+function myFunctionl4() {
+	window.location.replace("http://hot.securecamsinvite.com/?a=4267&c=446&s1=");
+	return true;
+}
